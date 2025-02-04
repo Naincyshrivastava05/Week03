0d1222d24d45a98daf3221e2c710964805c50f88
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-04 17:01:42 +0530

    [Naincy Shrivastava] add File Reader read line by line

---
+++ String-Manipulation/src/main/java/filereader/ReadLineByLine.java
@@ -0,0 +1,31 @@
+package filereader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadLineByLine {
+    public static String reader(String file){
+        String s = "";
+        try{
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String  line;
+            while((line = br.readLine())!=null){
+                s+=line;
+                s+="\n";
+            }
+        }catch (IOException e){
+            System.out.println("Error while reading file-"+ e.getMessage());
+            e.printStackTrace();
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        String path = "C:\\Gabgemini\\Week03\\String-Manipulation\\src\\main\\java\\filereader\\text.txt";
+        String fileText = reader(path);
+        System.out.println(fileText);
+
+    }
+
+}
